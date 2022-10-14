09460c3b96dae9054d86f0a57549a6b35502581d
Eric Opoku <eric.opoku20@yahoo.com>
2022-10-13 21:05:05 -0400

    initial commit

---
+++ src/main/java/com/developersboard/EtTransportApplication.java
@@ -0,0 +1,12 @@
+package com.developersboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtTransportApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EtTransportApplication.class, args);
+  }
+}
---
+++ src/test/java/com/developersboard/EtTransportApplicationTests.java
@@ -0,0 +1,11 @@
+package com.developersboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtTransportApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
